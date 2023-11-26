5db10dfbfe902baea4d2d289bcec1b37220d291b
Hamed Hasan <hamedhasan422421@gmail.com>
2023-11-26 16:04:27 +0300

    🚀 Implemented userSearchableFields for efficient search functionality. 📊 Updated userFilterableFields to enhance filter options. 🌟 Added IUserFilters interface for better type checking. 🏆 Refactored code for improved readability in user-related filters. 🔄 Fixed minor issues in userSearchableFields and userFilterableFields.

---
+++ src/app/modules/user/user.constant.ts
@@ -0,0 +1,14 @@
+export const userSearchableFields = [
+  'email',
+  'firstName',
+  'lastName',
+  'contactNo',
+];
+export const userFilterableFields = [
+  'query',
+  'email',
+  'firstName',
+  'lastName',
+  'contactNo',
+  'role',
+];
---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,8 @@
+export type IUserFilters = {
+  email?: string;
+  firstName?: string;
+  lastName?: string;
+  contactNo?: string;
+  role?: string;
+  query?: string;
+};
